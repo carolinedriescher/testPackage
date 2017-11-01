2795a681af006a9fcbf9e1145c5d6d516c6edd24
Caroline <driescher@130-229-8-99-dhcp.wlan.ki.se>
2017-11-01 14:14:23 +0100

    first commit

---
+++ R/testFun.R
@@ -0,0 +1,14 @@
+#' testFun
+#'
+#' @name testFun
+#' @rdname testFun
+#' @author Martin Enge
+#' @examples
+#'
+#' testFun('matrin')
+#'
+#' @export
+
+testFun <- function(name) {
+  print(paste("my name is: ", name, sep = " "))
+}
